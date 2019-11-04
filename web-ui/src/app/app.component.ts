06c00273a1511ecf5cd541265d71a14b9e8c388d
Ralph Florent <ralflornt@gmail.com>
2019-11-04 14:27:44 +0100

    feat: create an Angular-based web app

---
+++ web-ui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'so-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Smart Outlet';
+}
